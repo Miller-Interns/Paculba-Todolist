8639a258f294e4863f896105bb82b9b25d05f3b7
Heriel Kaye Paculba <paculba.herielkaye@gmail.com>
2025-07-11 11:56:23 +0800

    navbar

--- src/views/ToDo.vue
+++ src/views/ToDo.vue
@@ -1,21 +1,71 @@
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue';
-
+import buttonsFormat from '@/components/buttons-format.vue';
+import { useRouter } from 'vue-router'
 </script>
 
-
 <template>
-  <div class="center-content">
-      <HelloWorld msg="TO DO" />
-      
+  <nav class="navbar">
+    <div class="navbar-title">ToDo App</div>
+    <div class="navbar-links">
+      <router-link to="/">Home</router-link>
+      <!-- <router-link to="/todo">ToDo</router-link> -->
+    </div>
+  </nav>
+  <div class="full-height-container">
+    <div class="center-content">
+      <profileFormat msg="HERIEL KAYE U. PACULBA" date="July 10, 2025"></profileFormat>
+    </div>
   </div>
 </template>
 
-<style>
+<style scoped>
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 60px;
+  background: #1976d2;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 2rem;
+  box-sizing: border-box;
+  z-index: 1000;
+}
+
+.navbar-title {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.navbar-links {
+  display: flex;
+  gap: 1.5rem;
+}
+
+.navbar-links a {
+  color: #fff;
+  text-decoration: none;
+  font-weight: 500;
+  transition: color 0.2s;
+}
+
+.navbar-links a.router-link-exact-active {
+  text-decoration: underline;
+}
+
+.navbar-links a:hover {
+  color: #bbdefb;
+}
+
 .full-height-container {
-  height: 100vh;
+  margin-top: 60px;
+  height: calc(100vh - 60px);
   width: 100vw;
 }
+
 
 .center-content {
   display: flex;
@@ -23,6 +73,7 @@ import HelloWorld from '@/components/HelloWorld.vue';
   align-items: center;
   text-align: center;
   height: 100%;
+  width: 100%
 }
 
 .center-content h1 {
